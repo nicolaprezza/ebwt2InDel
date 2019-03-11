177ef8fc10bd6da03552f4e67c71884c7eac2b0b
nico <nicola.prezza@gmail.com>
2019-03-11 16:27:55 +0100

    new sam2vcf

--- sam2vcf.cpp
+++ sam2vcf.cpp
@@ -2,6 +2,17 @@
 // Use of this source code is governed
 // by a MIT license that can be found in the LICENSE file.
 
+/*
+ * First: obtain the KissReads2 file (.snp) with ebwt2snp. Convert it to fastq with seqtk:
+ *
+ * seqtk seq -F 'h' input.snp > output.fastq
+ *
+ * Then, align it with BWA-MEM against the reference genome. Finally, this piece of code converts all differences seen in the sam file
+ * into variants in a VCF file.
+ *
+ *
+ */
+
 #include <iostream>
 #include <fstream>
 #include <assert.h>
@@ -12,6 +23,7 @@
 #include <cstring>
 #include "include.hpp"
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
@@ -25,79 +37,76 @@ bool only_exact = false;
 void help(){
 
 	cout << "sam2vcf [OPTIONS]" << endl << endl <<
-	"Converts the aligned calls (with bwa-mem) 'calls.sam' of clust2snp into a vcf file 'calls.sam.vcf'." << endl <<
+	"Turns all mismatches/indels seen in a sam file into VCF entries." << endl <<
 	"Options:" << endl <<
 		"-h          Print this help." << endl <<
-		"-x          Disable non-isolated SNPs (default: enabled)." << endl <<
+		"-f <arg>    Reference fasta file. REQUIRED." << endl <<
 		"-s <arg>    Input SAM file. REQUIRED" << endl <<
-		"-d <arg>    Keep only one indel in pairs within <arg> bases. Default: " <<  indel_deduplicate_def << "." << endl <<
-		"-e          Keep only exact alignments." << endl;
+		"-v <arg>    Output vcf file. REQUIRED." << endl <<
+		"-m <arg>    Maximum number of differences, i.e. mismatches + indels (default:5)" << endl;
 	exit(0);
 }
 
-struct vcf_entry{
+void parse_cigar(string& cigar, int& x, int& y, int& z, char& ty){
 
-	string chr;
-	uint64_t pos;
+	int num[4];
+	char type[4];
 
-	string REF;
-	string ALT;
+	num[0]=0;
+	num[1]=0;
+	num[2]=0;
 
-	bool indel;
+	int i = 0;
 
-	bool exact;
+	for(char c:cigar){
 
-	uint64_t cov_ref;
-	uint64_t cov_alt;
+		if(i<4){
 
-	void print(){
+			if('0' <= c and c <= '9'){
 
-		cout << endl;
-		cout << chr << " " << pos << " " << REF << " " << ALT << " " << indel << " " << exact << " " << cov_ref << " " << cov_alt << endl;
+				num[i] = num[i]*10 + (int(c)-int('0'));
 
-	}
+			}else{
 
-	bool operator<(const vcf_entry & a) const{
-
-		if(chr.compare(a.chr) < 0 ) return true;
-		else if(chr.compare(a.chr) == 0 ){
-
-			if(pos == a.pos){
-
-				string snp1 = REF;
-				snp1.append(ALT);
-
-				string snp2 = a.REF;
-				snp2.append(a.ALT);
-
-				return snp1.compare(snp2) < 0;
+				type[i++] = c;
 
 			}
 
-			return pos < a.pos;
-
 		}
 
-		return false;
+	}
+
+	if(i==1 and type[0]=='M'){
+		x=num[0];
+		ty = 'M';
+		return;
+	}
+
+	if(i==3 and type[0]=='M' and (type[1]=='D' or type[1]=='I') and type[2]=='M'){
+
+		x = num[0];
+		y = num[1];
+		z = num[2];
+
+		ty = type[1];
+
+		return;
 
 	}
 
-	bool operator==(const vcf_entry & a) const{
-
-		return chr.compare(a.chr)==0 and pos == a.pos and REF.compare(a.REF) == 0 and ALT.compare(a.ALT) == 0;
-
-	}
-
-};
+}
 
 int main(int argc, char** argv){
 
 	if(argc < 2) help();
 
-	string infile;
+	string input_fasta;
+	string input_sam;
+	string output;
+	int max_mism = 5;
 
 	int opt;
-	while ((opt = getopt(argc, argv, "xhd:s:e")) != -1){
+	while ((opt = getopt(argc, argv, "hs:f:v:m:")) != -1){
 		switch (opt){
 			case 'h':
 				help();
@@ -111,8 +120,17 @@ int main(int argc, char** argv){
 			case 'd':
 				indel_deduplicate = atoi(optarg);
 			break;
+			case 'f':
+				input_fasta = string(optarg);
+			break;
 			case 's':
-				infile = string(optarg);
+				input_sam = string(optarg);
+			break;
+			case 'v':
+				output = string(optarg);
+			break;
+			case 'm':
+				max_mism = atoi(optarg);
 			break;
 			default:
 				help();
@@ -120,355 +138,180 @@ int main(int argc, char** argv){
 		}
 	}
 
-	indel_deduplicate = indel_deduplicate==0 ? indel_deduplicate_def : indel_deduplicate;
+	if(input_fasta.compare("")==0) help();
+	if(input_sam.compare("")==0) help();
+	if(output.compare("")==0) help();
 
-	if(infile.compare("")==0) help();
+	ifstream ifasta(input_fasta);
+	ifstream isam(input_sam);
+	ofstream out(output);
 
-	string outfile = infile;
-	outfile.append(".vcf");
+	//reference indexed by contig: e.g. string chr1 = ref["chr1"]
+	std::map<string,string> ref;
 
-	ifstream is(infile);
-	ofstream of(outfile);
+	cout << "Loading reference ... " << flush;
+
+	/*
+	 *
+	 * READ REFERENCE, LOAD CONTIGS IN MEMORY
+	 *
+	 */
+
+	string line;
+	string contig;
+
+	vector<string> contigs;
+
+	int nonisolated_snps=0;
+
+	while(not ifasta.eof()){
+
+		getline(ifasta, line);
+
+		if(line[0] == '>'){//new contig name
+
+			contig = line.substr(1);
+			contigs.push_back(contig);
+
+			ref[contig] = string();
+
+		}else{
+
+			for (auto & c: line) c = toupper(c);
+			ref[contig].append(line);
+
+		}
+
+	}
+
+	cout << "done." << endl;
+
+	cout << "Contig\tlength" << endl;
+	for(auto c : contigs){
+
+		cout << c << "\t" << ref[c].length() << endl;
+
+	}
+
+	ifasta.close();
 
 	string str;
+	out << "#CHROM\tPOS\tID\tREF\tALT\tTYPE" << endl;
 
-	of << "#CHROM\tPOS\tID\tREF\tALT\tTYPE\tEXACT\tCOV_REF\tCOV_ALT" << endl;
-	//cout << "#CHROM\tPOS\tID\tREF\tALT\tINFO" << endl;
-
-	vector<vcf_entry> VCF;
-
-	while(getline(is, str)){
+	while(getline(isam, str)){
 
 		if(str[0]!='@' and str[0]!='['){//skip header
 
 			string name;//entry name
+			string flag;
+			string chr;
+			string pos;
+			string mapq;
+			string cigar;
+			string rnext;
+			string pnext;
+			string tlen;
+			string seq;
+			string qual;
+			string NM;
+
 			std::istringstream iss(str);
 			getline(iss, name, '\t');
+			getline(iss, flag, '\t');
+			getline(iss, chr, '\t');
+			getline(iss, pos, '\t');
+			getline(iss, mapq, '\t');
+			getline(iss, cigar, '\t');
+			getline(iss, rnext, '\t');
+			getline(iss, pnext, '\t');
+			getline(iss, tlen, '\t');
+			getline(iss, seq, '\t');
+			getline(iss, qual, '\t');
+			getline(iss, NM, '\t');
 
-			string event_nr;//INDEL or SNP
-			string type;//INDEL or SNP
-			string REF;//reference allele
-			string ALT;//alternative allele
-			string REF_dna;//reference dna
-			string ALT_dna;//alternative dna
-			uint64_t pos;//alignment position
-			uint64_t COV_REF;//reads supporting variation on individual 1
-			uint64_t COV_ALT;//reads supporting variation on individual 2
-			int snp_pos;
-			string chr;
-			string cigar;
-			string mismatches;
+			uint64_t pos_int = atoi(pos.c_str());
 
-			string token;
+			int ID = 1; //event ID
 
-			//First individual = reference = read DNA
-			//Second individual = ALT = DNA in header
+			int n_mism = 0;
 
+			//get number of mismatches (includes indels and mismatches)
 			{
 
-			std::istringstream iss1(name);
+				std::istringstream stream(NM);
+				string nm_s;
+				getline(stream, nm_s, ':');
+				getline(stream, nm_s, ':');
+				getline(stream, nm_s, ':');
 
-			getline(iss1, type, '_');
-			getline(iss1, event_nr, '_');
-			getline(iss1, token, '_');
-			snp_pos = atoi(token.c_str());
-
-			string REFALT;
-			getline(iss1, REFALT, '_');
-
-			std::istringstream iss2(REFALT);
-			getline(iss2, REF, '/');
-			ALT=string();
-			getline(iss2, ALT, '/');
-
-			getline(iss1, token, '_');
-			COV_REF = atoi(token.c_str());
-			getline(iss1, token, '_');
-			COV_ALT = atoi(token.c_str());
-
-			getline(iss1, ALT_dna, '_');
+				n_mism = atoi(nm_s.c_str());
 
 			}
 
-			getline(iss, token, '\t');//flag
-			unsigned int f = atoi(token.c_str());
-			getline(iss, chr, '\t');
-			getline(iss, token, '\t');
-			pos = atoi(token.c_str());
-			getline(iss, token, '\t');
-			getline(iss, cigar, '\t');
+			//if mismatch/indel is present
+			if(n_mism>0 and n_mism <= max_mism and ref[chr].size()>0){
 
-			getline(iss, token, '\t');
-			getline(iss, token, '\t');
-			getline(iss, token, '\t');
-			getline(iss, REF_dna, '\t');
+				//only admitted cigar formats: xM or xMy{I,D}zM, where x,y,z are integers
 
-			getline(iss, token, '\t');//quality
+				int x=0,y=0,z=0;
+				char type = 0;
 
-			getline(iss, mismatches, '\t');//NM:i:x
+				parse_cigar(cigar,x,y,z,type);
 
-			//exact alignment: 0 mismatches and 0 skips/indels
-			bool exact = 	std::count(cigar.begin(), cigar.end(), 'S') == 0 and
-							std::count(cigar.begin(), cigar.end(), 'I') == 0 and
-							std::count(cigar.begin(), cigar.end(), 'D') == 0 and
-							mismatches.compare("NM:i:0")==0;
+				int k = 0;//position on read
 
-			bool reversed = (f & (unsigned int)16) != 0;
-			bool indel = type.compare("INDEL")==0;
+				if(x>0){
 
-			if(reversed){//then REF_DNA has been reverse-complemented by the aligner. apply reverse-complement also ALT_DNA and the variant
+					//scan the first x bases in REF and ALT
+					for(int j=0;j<x;++j){
 
-				ALT_dna = RC(ALT_dna);
-				REF = RC(REF);
-				ALT = RC(ALT);
+						if(ref[chr][pos_int + j - 1] != seq[k]){
 
-				if(indel){
+							out << chr << "\t" << (pos_int + j) << "\t" << (ID++) << "\t" << ref[chr][pos_int + j - 1] << "\t" << seq[k] << "\tSNP" << endl;
 
-					snp_pos--;
+						}
 
-				}
-
-			}
-
-
-			/*cout << "event_nr " << event_nr << endl;
-			cout << "type " << type<< endl;//INDEL or SNP
-			cout <<  "REF " <<  REF<< endl;//reference allele
-			cout <<  "ALT " <<  ALT<< endl;//alternative allele
-			cout <<  "REF_dna " <<  REF_dna<< endl;//reference dna
-			cout <<  "ALT_dna " <<  ALT_dna<< endl;//alternative dna
-			cout <<  "pos " <<  pos<< endl;//alignment position
-			cout <<  "COV_REF " <<  COV_REF<< endl;//reads supporting variation on individual 1
-			cout <<  "COV_ALT " <<  COV_ALT<< endl;//reads supporting variation on individual 2
-			cout <<  "snp_pos " <<  snp_pos<< endl;
-			cout <<  "chr " <<  chr<< endl;
-			cout <<  "cigar " <<  cigar<< endl;
-			cout <<  "mismatches " <<  mismatches<< endl;
-			cout <<  "flag " <<  f<< endl;
-			cout <<  "indel? " <<  indel<< endl;
-			cout <<  "reversed? " <<  reversed<< endl<< endl;*/
-
-
-			//adjust snp_pos in the case we are on FW strand
-			if(not reversed){
-
-				if(indel){
-
-					if(REF.length()>0){
-
-						//insert in REF
-						int indel_len = REF.length();
-						snp_pos = ((REF_dna.length() - snp_pos) - indel_len) -1;
-
-					}else{
-
-						//insert in ALT
-						snp_pos = (REF_dna.length() - snp_pos) - 1;
+						k++;
 
 					}
 
-				}else{
+					//if there's an indel
+					if(y>0){
 
-					snp_pos = (REF_dna.length() - snp_pos)-1;
+						//insert of length y in reference
+						if(type == 'I'){
 
-				}
+							string REF = ref[chr].substr(pos_int + x - 2,1);
 
-			}
+							string ALT = seq.substr(k-1,y+1);
 
-			if((f==0 or f==16) and snp_pos >= 0 and chr.compare("*") != 0){
+							k += y;
 
-				vcf_entry v;
+							out << chr << "\t" << (pos_int + (x-1)) << "\t" << (ID++) << "\t" << REF << "\t" << ALT << "\tINDEL" << endl;
 
-				if(indel){
+						}else if (type == 'D'){//deletion in reference
 
-					if(REF.length()>0){
+							string REF = ref[chr].substr(pos_int + x - 2,y+1);
+							string ALT = seq.substr(k-1,1);
+							out << chr << "\t" << (pos_int + (x-1)) << "\t" << (ID++) << "\t" << REF << "\t" << ALT << "\tINDEL" << endl;
 
-						assert(snp_pos<REF_dna.size());
-						assert(snp_pos<ALT_dna.size());
-
-						v = {
-										chr,
-										pos + snp_pos,
-										REF_dna.substr(snp_pos,REF.length()+1),
-										ALT_dna.substr(snp_pos,1),
-										true,
-										exact,
-										COV_REF,
-										COV_ALT
-						};
-
-					}else{
-
-						assert(snp_pos<REF_dna.size());
-						assert(snp_pos<ALT_dna.size());
-
-						v = {
-										chr,
-										pos + snp_pos,
-										REF_dna.substr(snp_pos,1),
-										ALT_dna.substr(snp_pos,ALT.length()+1),
-										true,
-										exact,
-										COV_REF,
-										COV_ALT
-						};
+						}
 
 					}
 
+					if(z>0){//scan the last z bases and look for further SNPs
 
-				}else{
+						for(int j=0;j<z;++j){
 
-					//cout << "SNP:\n" << REF_dna << endl << ALT_dna << endl << REF_dna.substr(snp_pos,1) << " " << ALT_dna.substr(snp_pos,1)  << endl;
+							uint64_t start = pos_int + x + (type=='D'?y:0) -1;//start is 0-based
 
-					v = {
-									chr,
-									pos + snp_pos,
-									REF,
-									ALT,
-									false,
-									exact,
-									COV_REF,
-									COV_ALT
-					};
+							if(ref[chr][start+j] != seq[k]){
 
-				}
-
-				if((not only_exact) or exact){
-
-					VCF.push_back(v);
-
-					/*
-					 * find non-isolated SNPs
-					 */
-
-					if(non_isolated){
-
-						if(indel){
-
-							if(reversed){
-
-								//non-isolated SNPs are on the right of the end position of indel
-
-								int indel_length = REF.length() > 0 ? REF.length() : ALT.length();
-
-								int L = std::max(REF_dna.length(), ALT_dna.length());//length of fragment containing the insert
-
-								int len_right = L - (snp_pos+indel_length) -1; //length of right part in common (with potential SNPs)
-
-								int snp_pos_ref = REF.length() > 0 ? snp_pos + indel_length +1 : snp_pos +1;
-								int snp_pos_alt = REF.length() > 0 ? snp_pos +1 : snp_pos + indel_length +1;
-
-								for(int i=0;i<len_right;++i){
-
-									if(REF_dna[snp_pos_ref+i] != ALT_dna[snp_pos_alt+i]){
-
-										assert(snp_pos_ref+i<REF_dna.size());
-										assert(snp_pos_alt+i<ALT_dna.size());
-
-										vcf_entry v  = {
-															chr,
-															pos + i,
-															REF_dna.substr(snp_pos_ref+i,1),
-															ALT_dna.substr(snp_pos_alt+i,1),
-															false,
-															exact,
-															COV_REF,
-															COV_ALT
-											};
-
-										VCF.push_back(v);
-
-									}
-
-								}
-
-							}else{
-
-								//non-isolated SNPs are on the left of the start position of indel
-
-								for(int i=0;i<snp_pos;++i){
-
-									if(REF_dna[i] != ALT_dna[i]){
-
-										assert(i<REF_dna.size());
-										assert(i<ALT_dna.size());
-
-										vcf_entry v  = {
-															chr,
-															pos + i,
-															REF_dna.substr(i,1),
-															ALT_dna.substr(i,1),
-															false,
-															exact,
-															COV_REF,
-															COV_ALT
-											};
-
-										VCF.push_back(v);
-
-									}
-
-								}
+								out << chr << "\t" << (start + j +1) << "\t" << (ID++) << "\t" << ref[chr][start+j] << "\t" << seq[k] << "\tSNP" << endl;
 
 							}
 
-						}else{
-
-							if(reversed){
-
-								//non-isolated SNPs are on the right
-
-								for(int i=snp_pos+1;i<REF_dna.length();++i){
-
-									if(REF_dna[i] != ALT_dna[i]){
-
-										//cout << "SNP (non IS):\n" << REF_dna << endl << ALT_dna << endl << REF_dna.substr(i,1) << " " << ALT_dna.substr(i,1)  << endl;
-
-										vcf_entry v  = {
-															chr,
-															pos + i,
-															REF,
-															ALT,
-															false,
-															exact,
-															COV_REF,
-															COV_ALT
-											};
-
-										VCF.push_back(v);
-
-									}
-
-								}
-
-							}else{
-
-								//non-isolated SNPs are on the left
-
-								for(int i=0;i<snp_pos;++i){
-
-									if(REF_dna[i] != ALT_dna[i]){
-
-										//cout << "SNP (non IS):\n" << REF_dna << endl << ALT_dna << endl << REF_dna.substr(i,1) << " " << ALT_dna.substr(i,1)  << endl;
-
-										vcf_entry v  = {
-															chr,
-															pos + i,
-															REF,
-															ALT,
-															false,
-															exact,
-															COV_REF,
-															COV_ALT
-											};
-
-										VCF.push_back(v);
-
-									}
-
-								}
-
-							}
+							k++;
 
 						}
 
@@ -481,78 +324,5 @@ int main(int argc, char** argv){
 		}
 
 	}
-
-	//remove duplicates
-
-	uint64_t old_size = VCF.size();
-
-	std::sort(VCF.begin(),VCF.end());
-
-	/*for(int i=0;i<VCF.size()-1;++i){
-
-		if(VCF[i]==VCF[i+1]){
-
-			cout << VCF[i].chr << "\t" << VCF[i].pos << "\t" << ".\t" << VCF[i].REF << "\t" << VCF[i].ALT << "\t" << (VCF[i].indel?"INDEL":"SNP") << endl;
-			cout << VCF[i+1].chr << "\t" << VCF[i+1].pos << "\t" << ".\t" << VCF[i+1].REF << "\t" << VCF[i+1].ALT << "\t" << (VCF[i+1].indel?"INDEL":"SNP") << endl;
-
-			cout << endl;
-
-		}
-
-	}*/
-
-	VCF.erase( unique( VCF.begin(), VCF.end() ), VCF.end() );
-
-	//remove close indels
-
-	vector<vcf_entry> VCF_filt;
-	for(int i=0;i<VCF.size()-1;++i){
-
-		if(		VCF[i].indel and
-				VCF[i+1].indel and
-				VCF[i].chr.compare(VCF[i+1].chr)==0 and
-				std::abs( int(VCF[i].pos)-int(VCF[i+1].pos) ) <= indel_deduplicate ){
-
-			VCF_filt.push_back(VCF[i]);//keep first indel
-			i++;//skip second indel
-
-		}else{
-
-			VCF_filt.push_back(VCF[i]);
-
-		}
-
-	}
-
-	VCF_filt.push_back(VCF[VCF.size()-1]);
-
-	uint64_t new_size = VCF_filt.size();
-
-	cout << (old_size-new_size) << " duplicates found. Saving remaining " << new_size << " unique SNPS/indels." << endl;
-
-	uint64_t n_indels = 0;
-	uint64_t n_snps = 0;
-
-	for(auto v:VCF_filt){
-
-		of	<< v.chr << "\t"
-			<< v.pos << "\t" << ".\t"
-			<< v.REF << "\t"
-			<< v.ALT << "\t"
-			<< (v.indel?"INDEL":"SNP") << "\t"
-			<< v.exact << "\t"
-			<< v.cov_ref << "\t"
-			<< v.cov_alt << endl;
-
-		n_indels += v.indel;
-		n_snps += (not v.indel);
-
-	}
-
-	cout << "Number of SNPs found: " << n_snps << endl;
-	cout << "Number of indels found: " << n_indels << endl;
-
-	is.close();
-	of.close();
 
 }
